ddbda3aa88ff743753262f09d206ba93fbea084c
Taylor Parrish <twpearish@gmail.com>
2020-03-17 20:29:06 -0700

    gets mean, median, and mode from a vector of integers

--- src/algorithms.rs
+++ src/algorithms.rs
@@ -1,20 +1,44 @@
 pub mod algo_funcs {
 
-    #[derive(Debug)]
-    pub enum FuncTypes {
-        Fibonacci
-    }
+    use std::collections::HashMap;
+    // Given a list of integers, find the mean, median, and mode
+    pub fn get_stats(mut targets: Vec<i32>) -> (f32, i32, i32) {
+        let mut sum = 0;
+        let target_count = targets.len();
+        let middle = target_count / 2;
 
-    pub fn write_output(func: FuncTypes, n: u16) {
-        let output = match func {
-            FuncTypes::Fibonacci => gen_fib(n)
+        println!("middle: {}", middle);
+        let mut number_counter = HashMap::new();
+
+        for t in &targets {
+            sum += t;
+            let count = number_counter.entry(t).or_insert(0);
+            *count += 1;
+        }
+        let mode = {
+            let mut max = 0;
+            let mut mode = 0;
+            for (k, v) in &number_counter {
+                if v >= &max {
+                    max = *v;
+                    mode = **k;
+                }
+            }
+            mode
         };
 
-        println!("{:?} with input {} results in {}", func, n, output);
+        println!("number counter: {:?}", number_counter);
+        let mean = sum as f32 / target_count as f32;
+        let median = {
+            targets.sort();
+            targets[middle]
+        };
+
+        (mean, median, mode)
     }
 
     // Generates nth fibonacci number
-    fn gen_fib(n: u16) -> u32 {
+    pub fn gen_fib(n: u16) -> u32 {
         let mut x = 1;
         let mut y = 0;
 
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,15 @@
 mod algorithms;
-mod geometry;
-mod temp;
+// mod geometry;
+// mod temp;
 
 use algorithms::algo_funcs;
-use geometry::geo_structs;
+// use geometry::geo_structs;
 
 fn main() {
-    temp::convert_temp();
-    geo_structs::rectangles();
-    algo_funcs::write_output(algo_funcs::FuncTypes::Fibonacci, 10);
+    // temp::convert_temp();
+    // geo_structs::rectangles();
+    println!("Fibonacci {} is: {}", 10, algo_funcs::gen_fib(10));
+
+    let (a, b, c) = algo_funcs::get_stats(vec![10, 4, 3, 8, 22, 36, 4, 6, 3]);
+    println!("Mean: {} -- Median: {} -- Mode: {}", a, b, c);
 }
